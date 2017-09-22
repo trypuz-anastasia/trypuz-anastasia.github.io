462c3cd2077fdc52d05a8fe86271f9f72454edb3
Anastasiya Trypuz <trypuz.anastasia@gmail.com>
2017-09-22 16:53:30 +0200

    ajax-drobne-zmiany

--- zadania-domowe/ajax/main.js
+++ zadania-domowe/ajax/main.js
@@ -2,7 +2,7 @@
 
 $( document ).ready( function(){
     
-    $ ("button").click( function() {
+    $ ('button').click( function() {
         
     $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function (data) {
         
@@ -10,9 +10,9 @@ $( document ).ready( function(){
             
         $('body').append('<div id="dane-programisty"></div>');
         $('<p>Imię: ' + data.imie + '</p>').appendTo('#dane-programisty');
-        $("<p>Nazwisko: " + data.nazwisko + "</p>").appendTo("#dane-programisty");
-        $("<p>Zawód: " + data.zawod + "</p>").appendTo("#dane-programisty");
-        $("<p>Firma: " + data.firma + "</p>").appendTo("#dane-programisty");
+        $('<p>Nazwisko: ' + data.nazwisko + '</p>').appendTo('#dane-programisty');
+        $('<p>Zawód: ' + data.zawod + '</p>').appendTo('#dane-programisty');
+        $('<p>Firma: ' + data.firma + '</p>').appendTo('#dane-programisty');
         });
     });
 });
